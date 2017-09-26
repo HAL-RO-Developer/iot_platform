1a17d1ff870dc2ffa1a1f856cec46923f0bececf
RikuHashimoto <hashimotoriku1@gmail.com>
2017-09-26 15:49:52 +0900

    I2Csetup追加

---
+++ device/source/end_func/I2Ctest/I2Ctest.ino
@@ -0,0 +1,29 @@
+#include <DeviceControl.h>
+#include"endfunc.h"
+
+CONTEXT_DATA context;
+RESULT_DATA  result;  
+
+SSHT I2Csetup( CONTEXT_DATA *cdata,RESULT_DATA *rdata);
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  SSHT rtn;
+  context.port[0] = 4; //SDA
+  context.port[1] = 5;//SCL
+  rtn = I2Csetup( &context,&result );
+  if(rtn == RESULT_OK){
+     Serial.println("I2C setup OK");
+    }
+}
+
+SSHT I2Csetup( CONTEXT_DATA *cdata,RESULT_DATA *rdata){
+    SSHT rtn = RESULT_OK;
+    I2C::begin(cdata->port[0],cdata->port[1]);
+     rdata->result = rtn;
+     return rtn;
+  }
+
+
