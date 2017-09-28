09a33d6d41bd81c71e83424798d0b0b14cfeb573
RikuHashimoto <hashimotoriku1@gmail.com>
2017-09-28 18:58:18 +0900

    I2CWrite作成

---
+++ device/source/end_func/I2CWritetest/I2CWrite.ino
@@ -0,0 +1,18 @@
+/* 
+  I2CWrite.ino
+   
+   I2C送信
+   
+   Created 2017/09/27
+   By Riku Hashimoto
+   */
+#include <DeviceControl.h>
+#include"endfunc.h"
+SSHT I2CWrite( SINT SleaveID, SINT data, RESULT_DATA *rdata) {
+  SSHT rtn = RESULT_OK;
+  rdata->result = rtn;
+  I2C::beginTransmission(SleaveID);
+  I2C::Write(data);
+  I2C::endTransmission();
+   return  rtn;
+  }
---
+++ device/source/end_func/I2CWritetest/I2CWritetest.ino
@@ -0,0 +1,22 @@
+//Slaveへ送信を行う
+#include <DeviceControl.h>
+#include"endfunc.h"
+char b='b';
+CONTEXT_DATA context;
+RESULT_DATA  result;  
+SSHT I2CSetup( CONTEXT_DATA *cdata,RESULT_DATA *rdata);
+SSHT I2CWrite( SINT SleaveID, SINT data, RESULT_DATA *rdata);
+void setup() {
+  context.port[0] = 4; //SDA
+  context.port[1] = 5;//SCL
+  I2CSetup( &context,&result );
+  Serial.begin(9600);
+}
+
+void loop() {
+    I2CWrite(2,b,&result);
+}
+
+
+
+
