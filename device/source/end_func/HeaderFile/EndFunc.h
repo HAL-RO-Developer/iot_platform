7f92da45044e75810b996fb21597f0ee5c617bbc
hiyanaka <nagaoka.hirotaka@gmail.com>
2017-09-29 10:13:03 +0900

    Update EndFunc.h

--- device/source/end_func/HeaderFile/EndFunc.h
+++ device/source/end_func/HeaderFile/EndFunc.h
@@ -22,13 +22,13 @@ typedef unsigned int   UINT;		/* unsigned int型   */
 
 /* --- 構造体宣言 --- */
 typedef struct{
-  SSHT port[2];  /* ポート情報 */
-  SSHT value;		 /* データ */
+  SSHT port[2];     /* ポート情報 */
+  SSHT value;		/* データ */
 } CONTEXT_DATA;
 
 typedef struct{
-  SSHT value;      /* データ */
-  SSHT result;   	 /* 実行結果(OK(0)/NG(1)) */
+  SSHT value;       /* データ */
+  SSHT result;      /* 実行結果(OK(0)/NG(1)) */
 } RESULT_DATA;
 
 /* --- 定数定義 --- */
