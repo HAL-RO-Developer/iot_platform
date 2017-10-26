16f868a5ce27bb873c7ed0dd1d1c3a462caa6116
Rikuhashimoto <rikuh>
2017-10-26 20:04:54 +0900

    ARead追加

--- device/source/end_func/ARead/onceARead/onceARead.ino
+++ device/source/end_func/ARead/onceARead/onceARead.ino
@@ -3,7 +3,7 @@ onceARead.ino
 
 アナログ入力を行う
 
-Created 2017/9/29
+Created 2017/10/25
 By Riku Hashimoto
 */
 
@@ -12,10 +12,10 @@ By Riku Hashimoto
 #include "EndFunc.h"
 SSHT onceARead( CONTEXT_DATA *cdata,RESULT_DATA *rdata){
     SSHT rtn = RESULT_OK;
-  if(cdata->port[0] != INPUT){
-Digital::SetMode(cdata->port[1], INPUT);
+  if(cdata->port.pin1 != INPUT){
+Digital::SetMode(cdata->port.pin1, INPUT);
  }
-rdata->value = Analog::Read(cdata->port[1]);//アナログ値取得
+rdata->value = Analog::Read(cdata->port.pin1);//アナログ値取得
 rdata->result = rtn;
 return rtn;
 }
---
+++ device/source/end_func/ARead/simpleARead/simpleARead.ino
@@ -0,0 +1,25 @@
+/*
+onceARead.ino
+
+アナログ入力を行う
+
+Created 2017/10/25
+By Riku Hashimoto
+*/
+
+
+#include <DeviceControl.h>
+#include "EndFunc.h"
+SSHT simpleARead( CONTEXT_DATA *cdata,RESULT_DATA *rdata,int ms){
+    SSHT rtn = RESULT_OK;
+  if(cdata->port.pin1 != INPUT){
+Digital::SetMode(cdata->port.pin1, INPUT);
+ }
+ static int Time = millis();
+if(millis()-Time >= ms ){
+rdata->value = Analog::Read(cdata->port.pin1);//アナログ値取得
+Time = millis();
+rdata->result = rtn;
+return rtn;
+}
+}
