2743c58e84d410a375c6f2f66251d5dd1e12a445
RikuHashimoto <hashimotoriku1@gmail.com>
2017-11-08 15:46:01 +0900

    Add files via upload

--- device/source/end_func/EndFunc/ef_AnalogWrite/ef_AnalogWrite.cpp
+++ device/source/end_func/EndFunc/ef_AnalogWrite/ef_AnalogWrite.cpp
@@ -1,7 +1,6 @@
 /*
 ef_AnalogWrite.cpp
-
-––’[ŠÖ” AnalogWrite
+æœ«ç«¯é–¢æ•° AnalogWrite
 
 Created 2017/10/07
 By  Riku Hashimoto
@@ -14,22 +13,23 @@ By  Riku Hashimoto
 #define Value (0)
 #define TIME  (1)
 
-SSHT blinkAWrite( CONTEXT_DATA *cdata,RESULT_DATA *rdata,SINT m){
+SSHT blinkAWrite( CONTEXT_DATA *cdata,RESULT_DATA *rdata) {
 SSHT rtn = RESULT_OK;
  if(cdata->port.mode != OUTPUT){
 Digital::SetMode(cdata->port.pin1, OUTPUT);
  }
+ 
  static SINT Time = millis();
-if(millis()-Time >= cdata->value[TIME]){
-Analog::Write(cdata->port.pin1, cdata->value[Value]);
-rdata->value = cdata->value[Value];
-rdata->result = rtn;
+if(millis()-Time >= cdata->value[TIME]) {
+  Analog::Write(cdata->port.pin1, cdata->value[Value]);
+  rdata->value = cdata->value[Value];
+  rdata->result = rtn;
  }
-if(millis()-Time >= cdata->value[TIME] *2){
-Analog::Write(cdata->port.pin1,0);
-rdata->value = 0;
-rdata->result = rtn;
-Time = millis();
+if(millis()-Time >= (cdata->value[TIME] *2)){
+   Analog::Write(cdata->port.pin1,0);
+   rdata->value = 0;
+    rdata->result = rtn;
+   Time = millis();
  }
 return rtn;
 }
@@ -54,13 +54,10 @@ SSHT simpleAWrite(CONTEXT_DATA *cdata, RESULT_DATA *rdata) {
 }
 
 SSHT onceAWrite(CONTEXT_DATA *cdata, RESULT_DATA *rdata) {
-
 	SSHT rtn = RESULT_OK;
 	if (cdata->port.mode != OUTPUT) {
 		Digital::SetMode(cdata->port.pin1, OUTPUT);
 	}
-
-
 	Analog::Write(cdata->port.pin1, cdata->value[Value]);
 	rdata->value = cdata->value[Value];
 	rdata->result = rtn;
