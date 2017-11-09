32966c35b9bfd2863623605c126d8a5a8ebd32b3
RikuHashimoto <hashimotoriku1@gmail.com>
2017-11-09 16:58:31 +0900

    Add files via upload

--- device/source/end_func/EndFunc/ef_AnalogWrite/ef_AnalogWrite.cpp
+++ device/source/end_func/EndFunc/ef_AnalogWrite/ef_AnalogWrite.cpp
@@ -21,15 +21,15 @@ Digital::SetMode(cdata->port.pin1, OUTPUT);
  
  static SINT Time = millis();
 if(millis()-Time >= cdata->value[TIME]) {
-  Analog::Write(cdata->port.pin1, cdata->value[Value]);
-  rdata->value = cdata->value[Value];
-  rdata->result = rtn;
+Analog::Write(cdata->port.pin1, cdata->value[Value]);
+rdata->value = cdata->value[Value];
+rdata->result = rtn;
  }
-if(millis()-Time >= (cdata->value[TIME] *2)){
-   Analog::Write(cdata->port.pin1,0);
-   rdata->value = 0;
-    rdata->result = rtn;
-   Time = millis();
+if(millis()-Time >= cdata->value[TIME] *2){
+Analog::Write(cdata->port.pin1,0);
+rdata->value = 0;
+rdata->result = rtn;
+Time = millis();
  }
 return rtn;
 }
@@ -54,6 +54,7 @@ SSHT simpleAWrite(CONTEXT_DATA *cdata, RESULT_DATA *rdata) {
 }
 
 SSHT onceAWrite(CONTEXT_DATA *cdata, RESULT_DATA *rdata) {
+
 	SSHT rtn = RESULT_OK;
 	if (cdata->port.mode != OUTPUT) {
 		Digital::SetMode(cdata->port.pin1, OUTPUT);
@@ -63,5 +64,4 @@ SSHT onceAWrite(CONTEXT_DATA *cdata, RESULT_DATA *rdata) {
 	rdata->result = rtn;
 	return rtn;
 }
-
-
+/* Copyright HAL College of Technology & Design */
