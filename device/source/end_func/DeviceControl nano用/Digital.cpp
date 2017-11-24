10f2af96445582da6c5bf722f8fb4c80bcaf346c
RikuHashimoto <hashimoto.riku1@gmail.com>
2017-11-24 14:54:04 +0900

    nano用追加
    nano用追加

---
+++ device/source/end_func/DeviceControl nano用/Digital.cpp
@@ -0,0 +1,18 @@
+#include "arduino.h"
+#include "DeviceControl.h"
+
+
+void Digital::SetMode( int pin, int mode )
+{
+	pinMode( pin, mode );
+}
+
+void Digital::Write( int pin, int value )
+{
+	digitalWrite( pin, value );
+}
+
+int Digital::Read( int pin )
+{
+	return digitalRead( pin );
+}
---
+++ device/source/end_func/DeviceControl nano用/UART.cpp
@@ -0,0 +1,77 @@
+#include "arduino.h"
+#include "DeviceControl.h"
+
+int UART::available( )
+{
+	return Serial.available( );
+}
+
+void UART::begin( unsigned long speed )
+{
+	Serial.begin( speed );
+}
+
+void UART::end( )
+{
+	Serial.end( );
+}
+
+int UART::Read( )
+{
+	return Serial.read( );
+}
+
+int UART::peek( )
+{
+	return Serial.peek( );
+}
+
+void UART::flush( )
+{
+	Serial.flush( );
+}
+
+int UART::Write( int value )
+{
+	return Serial.write( value );
+}
+
+int UART::Write( char* string )
+{
+	return Serial.write( string );
+}
+
+int UART::Write( unsigned char* buffer, int length )
+{
+	return Serial.write( buffer, length );
+}
+
+int UART::print( int value )
+{
+	return Serial.print( value );
+}
+
+int UART::print( char* string )
+{
+	return Serial.print( string );
+}
+
+int UART::print( int value, int format )
+{
+	return Serial.print( value, format );
+}
+
+int UART::println( int value )
+{
+	return Serial.println( value );
+}
+
+int UART::println( char* string )
+{
+	return Serial.print( string );
+}
+
+int UART::println( int value, int format )
+{
+	return Serial.println( value, format );
+}
