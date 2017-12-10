595647bc414af3dee72f16665ac13b30f62b06eb
show-t <tanaka.shogo2@gmail.com>
2017-12-11 00:16:58 +0900

    パスワード可視化切替追加

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,23 +1,37 @@
-function showSignIn(){
-    $(function(){
-        $('#signin').css('display','block');
-        $('#signup').css('display','none');
+function showSignIn() {
+    $(function () {
+        $('#signin').css('display', 'block');
+        $('#signup').css('display', 'none');
     });
 }
 
-function showSignUp(){
-    $(function(){
-        $('#signin').css('display','none');
-        $('#signup').css('display','block');
+function showSignUp() {
+    $(function () {
+        $('#signin').css('display', 'none');
+        $('#signup').css('display', 'block');
     });
 }
 
-$(function(){
+$(function () {
     showSignIn();
 });
 
-$(function(){
-    $('.signin-form').on('submit', function(event){
+$(function () {
+     $('.js-show').each(function () {
+        var $pass = $(this).find('.js-pass');
+        var $input = $(this).find('.js-check');
+        $input.change(function () {
+            if ($(this).prop('checked')) { 
+                $pass.attr('type', 'text'); 
+            } else { 
+                $pass.attr('type', 'password'); 
+            } 
+        }); 
+    }); 
+});
+
+$(function () {
+    $('.signin-form').on('submit', function (event) {
         event.preventDefault();
         $.ajax({
             url: $(this).attr('action'),
@@ -25,14 +39,17 @@ $(function(){
             data: $(this).serialize(),
             timeout: 10000,
         })
-        .done(function(data){
-            console.log(data)
-            console.log(data['token'])
-            $.cookie( 'token', data['token'], { expires: 1 })
-            location.href = '/dashboard';   // 仮
-        })
-        .fail(function(XMLHttpRequest, textStatus, errorThrown){
-            alert(data['error'])
-        });
+            .done(function (data) {
+                console.log(data)
+                console.log(data['token'])
+                $.cookie('token', data['token'], { expires: 1 })
+                location.href = '/dashboard';   // 仮
+            })
+            .fail(function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log(XMLHttpRequest)
+                console.log(textStatus)
+                console.log(errorThrown)
+                alert(XMLHttpRequest['responseJSON']['err'])
+            });
     });
 });
