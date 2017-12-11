3f569316a528bc1dfc08a94dc2b6b0ae8d55e154
show-t <tanaka.shogo2@gmail.com>
2017-12-11 19:33:52 +0900

    getDeviceList　未完成

--- server/public/js/dashboard.js
+++ server/public/js/dashboard.js
@@ -1,24 +1,42 @@
 $(function () {
-    if( $.cookie('token') == null ){
+
+    if ($.cookie('token') == '') {
         location.href = '/';
+    } else {
+        $('.default').css('display', 'block');
+        $('.contents').css('display', 'none');
     }
 });
 
-function signout(){
-    $(function(){
-        if(!confirm('サインアウトしますか?')){
+function signout() {
+    $(function () {
+        if (!confirm('サインアウトしますか?')) {
             /* キャンセルの時の処理 */
             return false;
-        }else{
+        } else {
             /*　OKの時の処理 */
-            $.removeCookie( 'token' );
+            $.removeCookie('token');
             location.href = '/';
         }
     });
 }
 
-$(function(){
-    $('#create-device-button').on('click',function(){
+function showCreateDevice() {
+    $('.default').css('display', 'none');
+    $('.contents').css('display', 'none');
+    $('#create-device').css('display', 'block');
+}
+
+function showListDevice() {
+    $('.default').css('display', 'none');
+    $('.contents').css('display', 'none');
+    $('#get-device').css('display', 'block');
+    var $response = getDevice();
+    console.log('response',$response);
+}
+
+$(function () {
+    $('#create-device-button').on('click', function () {
         $.ajax({
             url: '/api/device',
             type: 'post',
@@ -26,9 +44,12 @@ $(function(){
             timeout: 10000,
             headers: {
                 'Authorization': $.cookie('token')
-            }
+            },
+            data: {
+                'device_name': $('#device-name').val()
+            },
         })
-            .done(function(data){
+            .done(function (data) {
                 $pin = data['pin'];
                 $('#pin-code').val($pin)
             })
@@ -42,9 +63,36 @@ $(function(){
     });
 });
 
-$(function() {
-    $("#copy-pin-code").on("click", function() {
-      alert($("#pin-code").val());
+$(function () {
+    $("#copy-pin-code").on("click", function () {
+        alert($("#pin-code").val());
     });
-  });
-  
+});
+
+function getDevice() {
+    return $.ajax({
+        url: '/api/device',
+        type: 'get',
+        dataType: 'json',
+        timeout: 10000,
+        headers: {
+            'Authorization': $.cookie('token')
+        },
+        data: {
+            'device_name': $('#device-name').val()
+        }
+    })
+}
+
+getDevice().done(function (data) {
+   // 成功時
+})
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        // 失敗時
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
+        $errMsg = jqXHR['responseJSON']['err']
+        alert($errMsg);
+    });
+
--- server/public/js/main.js
+++ server/public/js/main.js
@@ -12,7 +12,7 @@ function showSignUp() {
     });
 }
 
-$(function () {
+$(function() {
     showSignIn();
 });
 
